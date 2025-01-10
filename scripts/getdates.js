3cf696223cee27fc78cbeeb2fc3db5f4f5424440
Pablo Palacios <25efilm.infobibliotecas@gmail.com>
2025-01-10 02:56:54 -0500

    Fixes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,12 @@
-// Insert current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+// Obtener el año actual y mostrarlo en el footer
+const currentYearSpan = document.querySelector("#currentyear");
+if (currentYearSpan) {
+  const currentYear = new Date().getFullYear();
+  currentYearSpan.textContent = currentYear;
+}
 
-// Insert last modified date
-document.getElementById(
-  "lastModified"
-).textContent = `Last modified: ${document.lastModified}`;
+// Mostrar la última fecha de modificación
+const lastModifiedDiv = document.querySelector("#lastModified");
+if (lastModifiedDiv) {
+  lastModifiedDiv.textContent = `Last Modification: ${document.lastModified}`;
+}
