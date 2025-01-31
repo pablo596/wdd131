39d2214bb73564d7ae0140b79109478b970a20bd
Pablo Palacios Álava <32400185+pablo596@users.noreply.github.com>
2025-01-31 15:25:47 -0500

    Fix country

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,31 @@
-function calculateWindChill(temp, wind) {
-  if (temp <= 10 && wind > 4.8) {
+// script.js
+function calculateWindChill(temp, windSpeed) {
+  if (temp <= 10 && windSpeed > 4.8) {
     return (
-      13.12 +
-      0.6215 * temp -
-      11.37 * Math.pow(wind, 0.16) +
-      0.3965 * temp * Math.pow(wind, 0.16)
-    ).toFixed(1);
+      (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(windSpeed, 0.16) +
+        0.3965 * temp * Math.pow(windSpeed, 0.16)
+      ).toFixed(1) + " °C"
+    );
   } else {
     return "N/A";
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const temp = parseFloat(document.getElementById("temp").textContent);
-  const wind = parseFloat(document.getElementById("wind").textContent);
-  const windChill = calculateWindChill(temp, wind);
-  document.getElementById("windChill").textContent = windChill;
+document.addEventListener("DOMContentLoaded", () => {
+  // Actualizar footer
+  document.getElementById("year").textContent = new Date().getFullYear();
+  document.getElementById("last-modified").textContent = document.lastModified;
 
-  const lastModified = new Date(document.lastModified);
-  document.getElementById("lastModified").textContent =
-    lastModified.toLocaleString();
+  // Calcular wind chill
+  const temp = parseFloat(document.getElementById("temp").textContent);
+  const windSpeed = parseFloat(
+    document.getElementById("wind-speed").textContent
+  );
+  document.getElementById("wind-chill").textContent = calculateWindChill(
+    temp,
+    windSpeed
+  );
 });
